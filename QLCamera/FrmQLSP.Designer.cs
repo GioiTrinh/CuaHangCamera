e38e8e6b930c311a416e1fe8041c7900247da7c1
Trịnh Văn Giỏi <1551010031gioi@ou.edu.vn>
2018-11-17 21:58:41 +0700

    update

--- QLCamera/FrmQLSP.Designer.cs
+++ QLCamera/FrmQLSP.Designer.cs
@@ -66,19 +66,6 @@
             // 
             // txtMaSanPham
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(181, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(217, 23);
-            this.textBox1.TabIndex = 0;
-            // 
-            // txtTenSanPham
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(181, 44);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(217, 23);
-            this.textBox2.TabIndex = 1;
             // 
             // panel1
             // 
